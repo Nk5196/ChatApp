08afcd4531b847412d167dab7a0ab18c65bae7c0
Nk5196 <narendrakandalkar@gmail.com>
2024-04-23 23:13:37 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './pages/login/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='p-4 h-screen flex items-center justify-center'>
+        <Login/>
+      </div>
+    </>
+  )
+}
+
+export default App
