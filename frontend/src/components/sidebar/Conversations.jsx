7b203959e5251bab997de5e1c3d16a4715e6aee2
Nk5196 <narendrakandalkar@gmail.com>
2024-04-29 00:27:03 +0530

    conversations

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+const Conversations = () => {
+  return (
+    <div className='py-2 w-56 flex flex-col mt-80'>
+    <Conversation/>
+    <Conversation/>
+    <Conversation/>
+    <Conversation/>
+    <Conversation/>
+    <Conversation/>
+    </div>
+  )
+}
+
+export default Conversations
--- frontend/src/components/sidebar/SideBar.jsx
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Conversations from './Conversations'
 import SearchInput from './SearchInput'
 
 const SideBar = () => {
   return (
-    <div>
+		<div className='border-r border-slate-500 p-4 flex flex-col'>
       <SearchInput/>
+      <div className='divider  px-3'>
+        <Conversations/>
+      </div>
     </div>
   )
 }
