da520e3665fc4f67cee72ed4f3e0bbe5d93f226d
Nk5196 <narendrakandalkar@gmail.com>
2024-04-29 00:26:51 +0530

    conversation ui

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Conversation = () => {
+  return (<>
+    <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+        <div className='avatar online'>
+            <div className='w-12 rounded-full'>
+                <img src='https://variety.com/wp-content/uploads/2021/04/Avatar.jpg?w=800' alt='user avatar'/>
+            </div>
+        </div>
+        <div className='flex flex-col flex-1'>
+            <div className='flex gap-3 justify-between'>
+                <p className='font-bold text-gray-200'>john Doe</p>
+                <span className='text-xl'>🙂</span>
+            </div>
+        </div>
+    </div>
+    <div className='divider my-0 py-0 h-1'></div></>
+  )
+}
+
+export default Conversation
