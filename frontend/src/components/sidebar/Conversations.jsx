98b7a7d6124be43bcf157e31a7b4c99c15b13aec
Nk5196 <narendrakandalkar@gmail.com>
2024-05-22 10:08:47 +0530

    conversations

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import useGetConversations from '../../hooks/useGetConversations';
 import Conversation from './Conversation'
 
 const Conversations = () => {
+  const {loading, conversations} = useGetConversations();
+  console.log("conversations-->>", conversations)
   return (
     <div className='py-2 flex flex-col mt-80'>
     <Conversation/>
