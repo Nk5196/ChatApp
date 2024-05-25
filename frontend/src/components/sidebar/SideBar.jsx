ba4fd723c38b64f3c4b7fc50641f822126aa5fae
Nk5196 <narendrakandalkar@gmail.com>
2024-05-25 23:53:31 +0530

    conversation page

--- frontend/src/components/sidebar/SideBar.jsx
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -1,38 +1,32 @@
-import React from 'react'
-import Conversations from './Conversations'
-import LogoutButton from './LogoutButton'
-import SearchInput from './SearchInput'
+import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
+import SearchInput from "./SearchInput";
 
-const SideBar = () => {
-  return (
+const Sidebar = () => {
+	return (
 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-      <SearchInput/>
-      <div className='divider  px-3'>
-        <Conversations/>
-      </div>
-        <LogoutButton/>
-    </div>
-  )
-}
+			<SearchInput />
+			<div className='divider px-3'></div>
+			<Conversations />
+			<LogoutButton />
+		</div>
+	);
+};
+export default Sidebar;
 
-export default SideBar
+// STARTER CODE FOR THIS FILE
+// import Conversations from "./Conversations";
+// import LogoutButton from "./LogoutButton";
+// import SearchInput from "./SearchInput";
 
-
-// import React from 'react'
-// import Conversations from './Conversations'
-// import LogoutButton from './LogoutButton'
-// import SearchInput from './SearchInput'
-
-// const SideBar = () => {
-//   return (
+// const Sidebar = () => {
+// 	return (
 // 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-//       <SearchInput/>
-//       <div className='divider  px-3'>
-//         <Conversations/>
-//       </div>
-//         <LogoutButton/>
-//     </div>
-//   )
-// }
-
-// export default SideBar
+// 			<SearchInput />
+// 			<div className='divider px-3'></div>
+// 			<Conversations />
+// 			<LogoutButton />
+// 		</div>
+// 	);
+// };
+// export default Sidebar;
